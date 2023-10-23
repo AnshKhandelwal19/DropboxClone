b089e528f6d9de15500f85dbf88911901a551206
Ansh <khandelwalansh8@gmail.com>
2023-10-23 03:46:33 -0700

    added webpages

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 #Route to Home Page
-@app.route("/")
-def hellow_world():
-    return render_template('home.html')
+@app.route("/files")
+def files():
+    return render_template('files.html')
+
+@app.route("/download")
+def downloads():
+    return render_template('download.html')
 
 #Run flask app
 if __name__ == "__main__":

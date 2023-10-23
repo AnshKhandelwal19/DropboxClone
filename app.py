8c56591e464c97aa537f107ac50efd6dc470c101
Ansh <khandelwalansh8@gmail.com>
2023-10-23 02:59:09 -0700

    created and loaded home page html

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#Route to Home Page
 @app.route("/")
 def hellow_world():
-    return "<p>Hello World</p>"
+    return render_template('home.html')
 
+#Run flask app
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
